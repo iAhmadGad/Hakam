8f02ba56f033de0337e466b3a87ff6657766f311
Ahmad Asaad <ahmadasaadh@gmail.com>
2024-06-01 23:33:43 +0300

    Print output with the number of each test + Print number of passed tests, wrong answers & runtime errors & Print 'Accepeted' if code passes all tests

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
-import sys, os, subprocess, multiline
+import sys
+from test import test
 
 RESET="\033[0m"
 BOLD="\033[1m"
@@ -6,46 +7,15 @@ RED="\033[31m"
 GREEN="\033[32m"
 LIGHT_WHITE="\033[97m"
 
+USAGE="Usage: python main.py test <path_to_test_list_file>"
+
 def main():
-    if len(sys.argv) < 3:
-        print("Usage: python main.py <some_argument> <path_to_test_file>")
-        return
+    if not len(sys.argv) == 3:
+        sys.exit(USAGE)
+    if sys.argv[1] != "test":
+    	sys.exit(USAGE)
     
-    f = open(sys.argv[2], "r")
-    testList = multiline.load(f)
-    f.close()
-
-    print("Compiling...")
-    if "compile" in testList:
-        compile_command = testList["compile"]
-        compile_result = subprocess.run(compile_command, shell=True)
-        if compile_result.returncode != 0:
-            print(f"Compilation failed with code {compile_result.returncode}")
-            return
-
-    print("Executing...")
-    if "execute" in testList:
-        for test in testList["tests"]:
-            input_data = test[0].encode().decode('unicode_escape')  # Interpret escape sequences
-            expected_output = test[1]
-            try:
-                result = subprocess.run(
-                    testList['execute'],
-                    input=input_data.encode(),
-                    stdout=subprocess.PIPE,
-                    stderr=subprocess.PIPE,
-                    shell=True
-                )
-                output = result.stdout.decode().strip()
-                if result.returncode != 0:
-                    print(f"Execution failed with code {result.returncode}")
-                    print(result.stderr.decode())
-                elif output == expected_output:
-                    print(f"{GREEN}Test Passed :){RESET}")
-                else:
-                    print(f"{RED}Wrong Answer :^)\n{RESET}expected {LIGHT_WHITE}{test[1]} {RESET}for input {LIGHT_WHITE}{test[0]} {RESET}not {RED}{output}")
-            except subprocess.CalledProcessError as e:
-                print(f"Command failed with error code {e.returncode}: {e.output.decode()}")
-
+    test(sys.argv[2])
+        
 if __name__ == "__main__":
     main()
