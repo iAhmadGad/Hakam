8e334c17e45f4ebb955b2a6d7d3644533781256a
Ahmad Asaad <ahmadasaadh@gmail.com>
2024-06-02 13:00:16 +0300

    Add new & help options

--- src/main.py
+++ src/main.py
@@ -8,14 +8,27 @@ GREEN="\033[32m"
 LIGHT_WHITE="\033[97m"
 
 USAGE="Usage: python main.py test <path_to_test_list_file>"
+HAKAMFILE='{\n\t"compile": ""\n\t,"execute": ""\n\t,"tests":\n\t[\n\t\t["", ""]\n\t\t,["", ""]\n\t]\n}'.expandtabs(4)
 
 def main():
-    if not len(sys.argv) == 3:
+    if sys.argv[1] == "test":
+        if len(sys.argv) == 2:
+            test("hakam.json")
+        else:
+            test(sys.argv[2])
+    elif sys.argv[1] == "new":
+        if len(sys.argv) == 2:
+            f = open("hakam.json", "w")
+            f.write(HAKAMFILE)
+            f.close()
+        else:
+            f = open(f"{sys.argv[2]}.json", "w")
+            f.write(HAKAMFILE)
+            f.close()
+    elif sys.argv[1] == "help":
         sys.exit(USAGE)
-    if sys.argv[1] != "test":
-    	sys.exit(USAGE)
-    
-    test(sys.argv[2])
+    else:
+        sys.exit(USAGE)
         
 if __name__ == "__main__":
     main()
