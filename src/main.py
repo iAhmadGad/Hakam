b5d19300bcfbe9b18b2c538fdf594bfc005d4caf
Ahmad Asaad <ahmadasaadh@gmail.com>
2024-06-08 12:59:59 +0300

    Update arguments handling using argparse

--- src/main.py
+++ src/main.py
@@ -1,40 +1,50 @@
-import sys
+import argparse
 from test import test
 
-RESET="\033[0m"
-BOLD="\033[1m"
-RED="\033[31m"
-GREEN="\033[32m"
-LIGHT_WHITE="\033[97m"
+RESET = "\033[0m"
+BOLD = "\033[1m"
+RED = "\033[31m"
+GREEN = "\033[32m"
+LIGHT_WHITE = "\033[97m"
 
-USAGE="Usage: hakam <command> [candidate] [option]\n\ncommands:\n\thelp\n\tnew : new test file with name 'testfile.json'\n\tnew [testfile] : new test file with name [testfile]\n\ttest : test file with name 'testfile.json'\n\ttest [testfile] : test file with name [testfile]\n\noptions:\n\t--strict : makes Hakam exits if your code answered wrong or if runtime error is thrown".expandtabs(4)
+TESTFILE = """{
+    "compile": "",
+    "execute": "",
+    "tests": [
+        ["", ""],
+        ["", ""]
+    ]
+}
+"""
 
-TESTFILE='{\n\t"compile": ""\n\t,"execute": ""\n\t,"tests":\n\t[\n\t\t["", ""]\n\t\t,["", ""]\n\t]\n}\n'.expandtabs(4)
+def create_test_file(filename):
+    with open(filename, "w") as f:
+        f.write(TESTFILE)
+    print(f"{LIGHT_WHITE}Created new test file: {filename}{RESET}")
 
+def run_tests(filename, strict, verbose):
+    test(filename, strict=strict, verbose=verbose)
+    
 def main():
-    if sys.argv[1] == "test":
-        if len(sys.argv) == 2:
-            test("testfile.json")
-        elif len(sys.argv) == 3:
-            if sys.argv[2] == "--strict":
-                test("testfile.json", strict = True)
-            else:
-                test(sys.argv[2])
-        else:
-            test(sys.argv[2], strict = True)
-    elif sys.argv[1] == "new":
-        if len(sys.argv) == 2:
-            f = open("testfile.json", "w")
-            f.write(TESTFILE)
-            f.close()
-        else:
-            f = open(sys.argv[2], "w")
-            f.write(TESTFILE)
-            f.close()
-    elif sys.argv[1] == "help":
-        sys.exit(USAGE)
-    else:
-        sys.exit(USAGE)
-        
+    parser = argparse.ArgumentParser(description="Hakam (Problem solving judge)")
+    subparsers = parser.add_subparsers(dest="command", required=True)
+
+    # New command
+    parser_new = subparsers.add_parser("new", help="Create a new test file")
+    parser_new.add_argument("--file", "-f", nargs="?", default="testfile.json", help="Name of the test file to create")
+
+    # Test command
+    parser_test = subparsers.add_parser("test", help="Run tests on a test file")
+    parser_test.add_argument("--file", "-f", nargs="?", default="testfile.json", help="Name of the test file to test")
+    parser_test.add_argument("--strict", "-s", action="store_true", help="Run tests in strict mode, which makes Hakam exit if your code didn't pass a test or a runtime error is thrown")
+    parser_test.add_argument("--verbose", "-v", action="store_true", help="Print all the tests")
+
+    args = parser.parse_args()
+
+    if args.command == "new":
+        create_test_file(args.file)
+    elif args.command == "test":
+        run_tests(args.file, args.strict, args.verbose)
+
 if __name__ == "__main__":
     main()
