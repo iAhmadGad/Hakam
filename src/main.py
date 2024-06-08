96551a97d079d3818b94dd6b210fd97caaf7d51d
Ahmad Asaad <ahmadasaadh@gmail.com>
2024-06-08 14:59:46 +0300

    Update command syntax & help meassages

--- src/main.py
+++ src/main.py
@@ -24,13 +24,13 @@ def main():
 
     # New command
     parser_new = subparsers.add_parser("new", help="Create a new test file")
-    parser_new.add_argument("--file", "-f", nargs="?", default="testfile.json", help="Name of the test file to create")
+    parser_new.add_argument("testfile", nargs="?", default="testfile.json", help="Name of the test file to create")
 
     # Test command
     parser_test = subparsers.add_parser("test", help="Run tests on a test file")
-    parser_test.add_argument("--file", "-f", nargs="?", default="testfile.json", help="Name of the test file to test")
-    parser_test.add_argument("--strict", "-s", action="store_true", help="Run tests in strict mode, which makes Hakam exit if your code didn't pass a test or a runtime error is thrown")
-    parser_test.add_argument("--verbose", "-v", action="store_true", help="Print all the tests")
+    parser_test.add_argument("testfile", nargs="?", default="testfile.json", help="Name of the test file to test")
+    parser_test.add_argument("--strict", "-s", action="store_true", help="exit if code answered wrong or if runtime error is thrown")
+    parser_test.add_argument("--verbose", "-v", action="store_true", help="print tests & results")
 
     args = parser.parse_args()
 
